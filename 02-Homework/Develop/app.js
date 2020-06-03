7f8d91723149bfdbdd5a4fdbe0af2df0db30ee2e
Sarah Wist <sarahwist107@gmail.com>
2020-06-02 21:33:20 -0700

    added switch case function and amended inquirer functions for employees types to reflect those changes along with an exit application function at the end

--- 02-Homework/Develop/app.js
+++ 02-Homework/Develop/app.js
@@ -10,86 +10,151 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-function render() {
-    inquirer.prompt([
-        {
-            name: "name",
-            type: "input",
-            message: "Type your name",
-        },
-        {
-            name: "id",
-            type: "input",
-            message: "Type your ID",
-        },
-        {
-            name: "email",
-            type: "input",
-            message: "Type your email",
-        },
-        {
-            name: "role",
-            type: "list",
-            message: "Employee Type:",
-            choices: ["Engineer", "Manager", "Intern"],
-        },
-
+const teamMembers = [];
+function render1() {
+  inquirer
+    .prompt([
+      {
+        name: "role",
+        type: "list",
+        message: "Employee Type:",
+        choices: ["Manager", "Engineer", "Intern", "NA"],
+      },
     ])
-        .then((answers) => {
-            const { Engineer, Manager, Intern } = answers;
-            if (answers === Engineer) {
-                prompt([
-                    {
-                        name: "github",
-                        type: "input",
-                        message: "Type your GitHub username",
-                    },
-                ]);
-            };
-            if (answers === Manager) {
-                prompt([
-                    {
-                        name: "officenumber",
-                        type: "input",
-                        message: "Type your office number",
-                    },
-                ]);
-            };
-            if (answers === Intern) {
-                prompt([
-                    {
-                        name: "school",
-                        type: "input",
-                        message: "Type your school name",
-                    },
-                ]);
-            };
-        });
 
-};
+    .then(function (choice) {
+      switch (choice.role) {
+        case "Manager":
+          addManager();
+          break;
+        case "Engineer":
+          addEngineer();
+          break;
+        case "Intern":
+          addIntern();
+          break;
+        default:
+          exitApplication();
+      }
+    });
+}
 
-render();
+render1();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+function addManager() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Type your name",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "Type your ID",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Type your email",
+      },
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+      {
+        name: "officeNumber",
+        type: "input",
+        message: "Type your office number",
+      },
+    ])
+    .then(function (answers) {
+      const manager = new Manager(
+        answers.name,
+        answers.id,
+        answers.email,
+        answers.officeNumber
+      );
+      teamMembers.push(manager);
+      console.log(answers);
+      render1();
+    });
+}
+function addEngineer() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Type your name",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "Type your ID",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Type your email",
+      },
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+      {
+        name: "github",
+        type: "input",
+        message: "Type your GitHub username",
+      },
+    ])
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+    .then(function (answerseng) {
+      const engineer = new Engineer(
+          answerseng.name, 
+          answerseng.id, 
+          answerseng.email, 
+          answerseng.github
+    );
+      teamMembers.push(engineer);
+      console.log(answerseng);
+      render1();
+    });
+}
+function addIntern() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "Type your name",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "Type your ID",
+      },
+      {
+        name: "email",
+        type: "input",
+        message: "Type your email",
+      },
+
+      {
+        name: "school",
+        type: "input",
+        message: "Type your school name",
+      },
+    ])
+
+    .then(function (answersint) {
+      const intern = new Intern(answersint.name, answersint.id, answersint.email, answersint.school);
+      teamMembers.push(intern);
+      console.log(answersint);
+      render1();
+    });
+}
+function exitApplication() {
+  const page = render(teamMembers);
+  fs.writeFile(outputPath, page, (err) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log("Succes!");
+  });
+}
